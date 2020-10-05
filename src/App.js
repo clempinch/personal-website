091a6f5f5b471a5b0d2ec8eecfebd8b6ac4d02e5
Clement Pinchedez <c.pinchedez@criteo.com>
2020-10-05 15:54:56 +0200

    ✨ Adding LinkedIn link.
    And group all social links in a div.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 class App extends React.Component {
 
@@ -11,9 +11,13 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <p>
-            <span role="img" aria-label="hello">👋</span> Hello, I'm Clément Pinchedez.</p>
+            <span role="img" aria-label="hello">👋</span> Hello, I'm Clément Pinchedez.
+          </p>
+          <div class="social">
             <a class="link" href="https://github.com/clempinch" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /> Github</a>
             <a class="link" href="https://twitter.com/clempinch" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /> Twitter</a>
+            <a class="link" href="https://www.linkedin.com/in/clementpinchedez/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /> LinkedIn</a>
+          </div>
         </header>
       </div>
     )
