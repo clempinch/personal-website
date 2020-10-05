7c6099ad3787773045cce9667484b3548354193f
Clement Pinchedez <c.pinchedez@criteo.com>
2020-10-05 15:16:00 +0200

    ✨ Adding Twitter link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 class App extends React.Component {
 
@@ -12,7 +12,8 @@ class App extends React.Component {
         <header className="App-header">
           <p>
             <span role="img" aria-label="hello">👋</span> Hello, I'm Clément Pinchedez.</p>
-            <a class="github" href="https://github.com/clempinch" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /> Github</a>
+            <a class="link" href="https://github.com/clempinch" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /> Github</a>
+            <a class="link" href="https://twitter.com/clempinch" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /> Twitter</a>
         </header>
       </div>
     )
